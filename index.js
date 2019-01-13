a0efe7df1284a9913e2c12b1084c7ce212023ffa
GabeKlavans <32149200+gabeklavans@users.noreply.github.com>
2019-01-13 02:02:29 -0500

    updated fields

--- index.js
+++ index.js
@@ -20,8 +20,8 @@ app.get('/index', function (req, res) {
     client.messages
     .create({
         body: text,
-        from: '+13217104809',
-        to: '+14074084325'
+        from: process.env.FROM_NUMBER,
+        to: process.env.TO_NUMBER
     })
     .then(message => console.log(message.sid))
     .done();
